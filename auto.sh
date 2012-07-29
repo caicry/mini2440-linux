c01709280be0d2ea199069a3306478eea55a21cf
Mr.C <caicry@gmail.com>
2012-07-29 07:38:54 -0700

    udisk build in kernel

---
+++ auto.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e 
+INSTALL_DIR=/tftproot/mini2440/
+if [ "x$CROSS_COMPILE" = 'x' ]  
+then
+    echo "import build mini2440 env"
+    . ./build_mini2440_env.sh
+    make mini2440_defconfig
+fi
+make -j 2
+
+cp -av arch/arm/boot/zImage $INSTALL_DIR/
---
+++ build_mini2440_env.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export CROSS_COMPILE=/opt/FriendlyARM/toolschain/4.4.3/bin/arm-linux-
+export ARCH=arm
+#make
